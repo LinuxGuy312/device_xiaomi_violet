fc82c154e158b1d940e236cc6f62edf557625d38
Eren <101087324+LinuxGuy312@users.noreply.github.com>
2025-02-21 09:45:23 +0000

    violet: @ProjectMatrixx Bringup
    * Add Maintainer Name
    * Update Partition Sizes to build vanilla
    * Remove BCR

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,5 @@ git clone --depth=1 https://github.com/selfmusing/platform_vendor_xiaomi_violet.
 git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
-git clone https://github.com/DerpFest-Devices/hardware_xiaomi.git -b 15 hardware/xiaomi
+git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 15.0 hardware/xiaomi
 
