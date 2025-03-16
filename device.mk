ffe5b01b8ac719e30f77bfae48fa17be9b6ed3a8
selfmusing <mirandamehek@gmail.com>
2025-03-17 03:54:17 +0530

    Revert "violet: Enable logcat service"
    This reverts commit b64b4f1b63685dcf5572659e58f6678162a337aa.

--- device.mk
+++ device.mk
@@ -383,9 +383,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
-    ueventd.qcom.rc \
-    logcat.rc
-
+    ueventd.qcom.rc
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
