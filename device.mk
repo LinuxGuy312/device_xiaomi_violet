819bf6c8eabfd83d871140df3b1d19888fcc8411
Eren <101087324+LinuxGuy312@users.noreply.github.com>
2025-04-09 15:44:51 +0000

    violet: Use libprotobuf-cpp-{full,lite}-v29 from compat

--- device.mk
+++ device.mk
@@ -408,14 +408,14 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
     libprotobuf-cpp-full \
+    libprotobuf-cpp-full-v29 \
+    libprotobuf-cpp-lite-v29 \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     librmnetctl \
     libxml2 \
     libsqlite.vendor
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
     prebuilts/vndk/v34/arm/arch-arm-armv7-a-neon/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcrypto-v34.so \
     prebuilts/vndk/v34/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v34.so
 
