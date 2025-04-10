4be8c7e55ad79ee0ef763a14bb6dd0f693f3b8d1
Eren <101087324+LinuxGuy312@users.noreply.github.com>
2025-04-10 17:13:43 +0000

    violet: Add checks to prevent re-cloning existing repositories

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,34 @@
-git clone --depth=1 https://github.com/selfmusing/platform_vendor_xiaomi_violet.git -b fifteen vendor/xiaomi/violet
-git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
-git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
-git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-git clone https://github.com/HorizonDroidLab/hardware_xiaomi -b fifteen hardware/xiaomi
+# Clone vendor
+if [ -d "vendor/xiaomi/violet" ]; then
+    echo "Directory vendor/xiaomi/violet already exists. Skipping clone."
+else
+    git clone --depth=1 -b fifteen https://github.com/selfmusing/platform_vendor_xiaomi_violet.git vendor/xiaomi/violet
+fi
+
+# Clone kernel
+if [ -d "kernel/xiaomi/violet" ]; then
+    echo "Directory kernel/xiaomi/violet already exists. Skipping clone."
+else
+    git clone -b main https://github.com/selfmusing/kernel_xiaomi_violet.git kernel/xiaomi/violet
+fi
+
+# Clone firmware
+if [ -d "vendor/xiaomi-firmware/violet" ]; then
+    echo "Directory vendor/xiaomi-firmware/violet already exists. Skipping clone."
+else
+    git clone --depth=1 -b 14.0 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git vendor/xiaomi-firmware/violet
+fi
+
+# Clone MiuiCamera
+if [ -d "vendor/MiuiCamera" ]; then
+    echo "Directory vendor/MiuiCamera already exists. Skipping clone."
+else
+    git clone --depth=1 -b 15 https://github.com/selfmusing/meme_cam.git vendor/MiuiCamera
+fi
+
+# Clone hardware/xiaomi
+if [ -d "hardware/xiaomi" ]; then
+    echo "Directory hardware/xiaomi already exists. Skipping clone."
+else
+    git clone -b fifteen https://github.com/HorizonDroidLab/hardware_xiaomi hardware/xiaomi
+fi
